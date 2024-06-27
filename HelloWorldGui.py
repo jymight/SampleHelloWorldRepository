872b45b423a98284df687c60f8ad7c55dd47b8b1
jymight <118308743+jymight@users.noreply.github.com>
2024-06-26 20:12:41 -0500

    Updated by Jyair

--- HelloWorldGui.py
+++ HelloWorldGui.py
@@ -1,10 +1,10 @@
 import tkinter
 
 window = tkinter.Tk()
-window.title("Hello World Program")
-window.geometry("1920x1080")
+window.title("Hello Jyair")
+window.geometry("1280x720")
 
-hello_message = tkinter.Message(window, text="Hello World!")
+hello_message = tkinter.Message(window, text="Hello Jyair")
 hello_message.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 window.mainloop()
