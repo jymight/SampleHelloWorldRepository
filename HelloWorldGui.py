1b58dd814f621ba135e3a93bc3bc980034d328e6
BrandonRiv <93412516+BrandonRiv@users.noreply.github.com>
2024-06-26 20:10:36 -0500

    Push Brandon

--- HelloWorldGui.py
+++ HelloWorldGui.py
@@ -4,7 +4,7 @@ window = tkinter.Tk()
 window.title("Hello World Program")
 window.geometry("1920x1080")
 
-hello_message = tkinter.Message(window, text="Hello World!")
+hello_message = tkinter.Message(window, text="Hello World! It is me Brandon.")
 hello_message.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 window.mainloop()
