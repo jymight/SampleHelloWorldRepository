36163f799a2747736efed873b0614f7e66007dcd
Chase Borchers <chaseborchers34@gmail.com>
2024-06-26 20:20:05 -0500

    Update HelloWorldGui.py

--- HelloWorldGui.py
+++ HelloWorldGui.py
@@ -1,10 +1,10 @@
 import tkinter
 
 window = tkinter.Tk()
-window.title("Hello World Program")
+window.title("Hello Chase's World")
 window.geometry("1920x1080")
 
-hello_message = tkinter.Message(window, text="Hello World!")
+hello_message = tkinter.Message(window, text="Hello Chase's World!")
 hello_message.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 window.mainloop()
